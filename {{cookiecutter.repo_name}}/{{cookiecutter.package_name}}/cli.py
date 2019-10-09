73e3ab4bf8ce8ea24b8919846a3c42ac3c5d7f50
Mike Sarahan <msarahan@gmail.com>
2019-10-09 12:01:17 -0500

    raw compiler jinja; add argparse skeleton

--- {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.package_name}}/cli.py
@@ -1,5 +1,28 @@
-def cli():
-    print("CLI template")
+from argparse import ArgumentParser
+from {{ cookiecutter['package_name'] }} import __version__
+
+def cli(args=None):
+    p = ArgumentParser(
+        description="{{ cookiecutter['project_short_description']}}",
+        conflict_handler='resolve'
+    )
+    p.add_argument(
+        '-V', '--version',
+        action='version',
+        help='Show the conda-prefix-replacement version number and exit.',
+        version="{{ cookiecutter['package_name']}} %s" % __version__,
+    )
+
+    args = p.parse_args(args)
+
+    # do something with the args
+    print("CLI template - fix me up!")
+
     # No return value means no error.
     # Return a value of 1 or higher to signify an error.
     # See https://docs.python.org/3/library/sys.html#sys.exit
+
+
+if __name__ == '__main__':
+    import sys
+    cli(sys.argv[1:])
