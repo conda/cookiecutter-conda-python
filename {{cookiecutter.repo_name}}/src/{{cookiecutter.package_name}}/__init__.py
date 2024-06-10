0655576fc0b327ef9283f29dc0546bb763f93ccf
Albert DeFusco <adefusco@anaconda.com>
2024-06-10 12:04:10 -0500

    100% coverage

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,4 +1,4 @@
 try:
     from {{ cookiecutter.package_name }}._version import version as __version__
-except ImportError:  # pragma: nocover
+except ImportError:  # pragma: no cover
     __version__ = "unknown"
